bc4f5812e4c6155b588be93d74f0dd900c66ee95
Ryan Morrissey <rmorrissey@netsuite.com>
2013-03-27 20:28:53 -0400

    Merge branch 'master' of https://github.com/23maverick23/oaswitches
    Conflicts:
    	oaswitches.js

--- oaswitches.js
+++ oaswitches.js
@@ -18,7 +18,7 @@
 				var label = "";
 
 				if (switchArray[i].getElementsByTagName("label").length > 0) {
-					label = switchArray[i].getElementsByTagName("label")[1].innerText;
+					label = switchArray[i].getElementsByTagName("label")[1].textContent;
 				}
 				else {
 					label = "-BLANK-";
